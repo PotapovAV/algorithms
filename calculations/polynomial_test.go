b29a28891a4a39e5eb6076502b776fa9dc2099b3
Artem Potapov <artem@jstick.ru>
2020-12-11 01:18:30 +0300

    Horner's scheme test added

---
+++ calculations/polynomial_test.go
@@ -0,0 +1,12 @@
+package calculations
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHornerScheme(t *testing.T) {
+	coefficients := []float64{2.0, 4.0, 7.0, 3.0}
+	assert.Equal(t, float64(3742), HornerScheme(coefficients, 10))
+}
